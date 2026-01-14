40bff130fe3b86a2be50cfa0f64892112ea47ebf
Joe Milburn <jrmilburn@outlook.com>
2026-01-14 21:29:13 +1000

    make landing publicly accessible

--- fitness-app/middleware.ts
+++ fitness-app/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/pricing"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/pricing", "/"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
